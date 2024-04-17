253553ba2c6e469d2cb789e84a83b4dde2d72c9d
kovpal322 <145595168+kovpal322@users.noreply.github.com>
2024-04-18 01:12:34 +0200

    mukodo chat

--- Animehub/src/pages/Chat.jsx
+++ Animehub/src/pages/Chat.jsx
@@ -1,20 +1,16 @@
-import {
-  MultiChatWindow,
-  MultiChatSocket,
-  useMultiChatLogic,
-} from "react-chat-engine-advanced";
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
 const Chat = (props) => {
-  const chatProps = useMultiChatLogic(
-    "777492b2-c259-4945-a6e3-6bed0f3b048e",
-    props.chatUser.username,
-    "secret"
-  );
   console.log(props.chatUser.secret);
+  
   return (
-    <div style={{ height: "100vh" }}>
-      <MultiChatWindow {...chatProps} />
-      <MultiChatSocket {...chatProps} />
+    <div className="fixed-top" style={{ height: '100vh' }}>
+      <PrettyChatWindow
+      projectId="777492b2-c259-4945-a6e3-6bed0f3b048e"
+      username= {props.chatUser.username}
+      secret="secret"
+      
+    />
     </div>
   );
 };
